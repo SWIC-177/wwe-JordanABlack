d889a0d8a9d9776e8e857d67c9acfb1f393334d7
JordanABlack <Jordanblack1120@gmail.com>
2024-04-25 23:42:40 -0500

    Homework Due Thursday II

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-import { CHAMPIONS } from "./src/data";
+import { BELTS } from "./src/data";
 
-const championsWithoutVacantTitle = CHAMPIONS.filter(
-    (champions) => champions !== "Vacant Title",
+const beltsWithoutTeamChampions = BELTS.filter(
+    (belts) => belt !== "World Tag Team Champions", "SmackDown Tag Team Champions", "WWE Women's Tag Team Champions",
 );
 
-console.log(championsWithoutVacantTitle);
+console.log(beltsWithoutTeamChampions);
+
+import { CHAMPIONS } from "./src/data";
+
+const championsCorrispondingIndex = CHAMPIONS.filter(
+    (champions) => champions !==  "Damian Priest",
+    "Vacant Title",
+    "Bayley",
+);
+
+console.log(championsCorrispondingIndex);
+
