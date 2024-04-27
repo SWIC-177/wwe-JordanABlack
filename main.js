f64f072dddf66e08436f82c926205353e41f424c
JordanABlack <Jordanblack1120@gmail.com>
2024-04-27 13:19:55 -0500

    Import champions

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-import { BELTS } from "./src/data";
-
-const beltsWithoutTeamChampions = BELTS.filter(
-    (belts) => belt !== "World Tag Team Champions", "SmackDown Tag Team Champions", "WWE Women's Tag Team Champions",
-);
-
-console.log(beltsWithoutTeamChampions);
-
 import { CHAMPIONS } from "./src/data";
 
-const championsCorrispondingIndex = CHAMPIONS.filter(
-    (champions) => champions !==  "Damian Priest",
-    "Vacant Title",
-    "Bayley",
-);
-
-console.log(championsCorrispondingIndex);
-
+console.log(CHAMPIONS);
