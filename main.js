1ddfa613799807f500095c2e226c5aeaae7a36a2
JordanABlack <Jordanblack1120@gmail.com>
2024-04-30 13:08:47 -0500

    Array of objects called 'CHAMPIONSHIPS'

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-import { CHAMPIONS } from "./src/data";
-
-const sortedChampions = CHAMPIONS.toSorted();
-
-console.log(sortedChampions);
+const CHAMPIONSHIPS = [
+    {
+      title: "NBA Championship",
+      champion: "Los Angeles Lakers"
+    },
+    {
+      title: "Super Bowl",
+      champion: "Tampa Bay Buccaneers"
+    },
+    {
+      title: "FIFA World Cup",
+      champion: "France"
+    },
+    
+  ];
+  
+  console.log(CHAMPIONSHIPS[0].title); 
+  console.log(CHAMPIONSHIPS[0].champion);
+  console.log(CHAMPIONSHIPS[1].title); 
+  console.log(CHAMPIONSHIPS[1].champion);
+  console.log(CHAMPIONSHIPS[2].title); 
+  console.log(CHAMPIONSHIPS[2].champion);   
